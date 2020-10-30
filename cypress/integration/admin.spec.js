cdd84206b08dda98ffba2e95a686d5e9d62e0afb
Jacob Smith <jacob.smith@automattic.com>
2020-10-30 10:48:32 -0700

    prettier update to match other 18c config

--- cypress/integration/admin.spec.js
+++ cypress/integration/admin.spec.js
@@ -1,49 +1,51 @@
 describe('WordPress Admin Tests', () => {
-  context('Login ahead of tests', () => {
-    beforeEach(() => {
-      // login before each test
-      cy.loginByForm();
-    });
+	context('Login ahead of tests', () => {
+		beforeEach(() => {
+			// login before each test
+			cy.loginByForm();
+		});
 
-    it('can visit /wp-admin/', () => {
-      // after cy.request, the session cookie has been set
-      // and we can visit a protected page
-      cy.visit('/wp-admin/');
-      cy.get('#contextual-help-link').should('contain', 'Help');
-    });
+		it('can visit /wp-admin/', () => {
+			// after cy.request, the session cookie has been set
+			// and we can visit a protected page
+			cy.visit('/wp-admin/');
+			cy.get('#contextual-help-link').should('contain', 'Help');
+		});
 
-    it('can visit /wp-admin/options-general.php and read options', () => {
-      // or another protected page
-      cy.visit('/wp-admin/options-general.php');
-      cy.get('.wrap h1').should('contain', 'General Settings');
-      cy.get('input[name="blogname"]').invoke('val').should('not.be.empty');
-    });
-  });
+		it('can visit /wp-admin/options-general.php and read options', () => {
+			// or another protected page
+			cy.visit('/wp-admin/options-general.php');
+			cy.get('.wrap h1').should('contain', 'General Settings');
+			cy.get('input[name="blogname"]')
+				.invoke('val')
+				.should('not.be.empty');
+		});
+	});
 
-  context('Test rewrite rules', () => {
-    beforeEach(() => {
-      // login before each test
-      cy.loginByForm();
-    });
+	context('Test rewrite rules', () => {
+		beforeEach(() => {
+			// login before each test
+			cy.loginByForm();
+		});
 
-    it('can visit /wp-admin/options-permalink.php', () => {
-      // after cy.request, the session cookie has been set
-      // and we can visit a protected page
-      cy.visit('/wp-admin/options-permalink.php');
-      cy.get('#contextual-help-link').should('contain', 'Help');
-    });
+		it('can visit /wp-admin/options-permalink.php', () => {
+			// after cy.request, the session cookie has been set
+			// and we can visit a protected page
+			cy.visit('/wp-admin/options-permalink.php');
+			cy.get('#contextual-help-link').should('contain', 'Help');
+		});
 
-    it('can visit /wp-admin/options-general.php and write options', () => {
-      // let's ensure that the permalinks are set to the correct setting, and in this case, force.
-      cy.visit('/wp-admin/options-permalink.php');
-      cy.get('.wrap h1').should('contain', 'Permalink Settings');
-      cy.get('form')
-        .find('[name="selection"]')
-        .check(['/%year%/%monthnum%/%postname%/']);
-      cy.get('#submit').click();
-      cy.get('div.notice')
-        .should('be.visible')
-        .and('contain', 'Permalink structure updated.');
-    });
-  });
+		it('can visit /wp-admin/options-general.php and write options', () => {
+			// let's ensure that the permalinks are set to the correct setting, and in this case, force.
+			cy.visit('/wp-admin/options-permalink.php');
+			cy.get('.wrap h1').should('contain', 'Permalink Settings');
+			cy.get('form')
+				.find('[name="selection"]')
+				.check(['/%year%/%monthnum%/%postname%/']);
+			cy.get('#submit').click();
+			cy.get('div.notice')
+				.should('be.visible')
+				.and('contain', 'Permalink structure updated.');
+		});
+	});
 });
