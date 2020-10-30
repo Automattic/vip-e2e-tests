418b08dbf4c293732ebbabed7a3292cb73d74c2b
Jacob Smith <jacob.smith@automattic.com>
2020-10-29 21:23:48 -0700

    updates to config

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,22 +1,26 @@
-describe('Logging In - WordPress Admin', () => {
+describe("Logging In - WordPress Admin", () => {
   // we can use these values to log in
-  const username = 'admin';
-  const password = 'password';
+  const username = Cypress.env("username");
+  const password = Cypress.env("password");
 
-  context('Unauthorized', () => {
-    it('is redirected on visit to /wp-login.php when no session', () => {
+  // it is ok for the username to be visible in the Command Log
+  expect(username, "username was set").to.be.a("string").and.not.be.empty;
+  // but the password value should not be shown
+  if (typeof password !== "string" || !password) {
+    throw new Error("Missing password value, set in cypress.env.json");
+  }
+
+  context("Unauthorized", () => {
+    it("is redirected on visit to /wp-login.php when no session", () => {
       // we must have a valid session cookie to be logged
       // in else we are redirected to /unauthorized
-      cy.visit('/wp-admin/');
-      cy.get('h1').should(
-        'contain',
-        'Powered by WordPress',
-      );
+      cy.visit("/wp-admin/");
+      cy.get("h1").should("contain", "Powered by WordPress");
 
-      cy.url().should('include', 'redirect_to');
+      cy.url().should("include", "redirect_to");
     });
 
-    it('is redirected using cy.request', () => {
+    it("is redirected using cy.request", () => {
       // instead of visiting the page above we can test this by issuing
       // a cy.request, checking the status code and redirectedToUrl property.
 
@@ -25,7 +29,7 @@ describe('Logging In - WordPress Admin', () => {
       // the 'redirectedToUrl' property is a special Cypress property under the hood
       // that normalizes the url the browser would normally follow during a redirect
       cy.request({
-        url: '/wp-admin/',
+        url: "/wp-admin/",
         followRedirect: false, // turn off following redirects automatically
       }).then((resp) => {
         // should have status code 302
@@ -34,48 +38,57 @@ describe('Logging In - WordPress Admin', () => {
         // when we turn off following redirects Cypress will also send us
         // a 'redirectedToUrl' property with the fully qualified URL that we
         // were redirected to.
-        expect(resp.redirectedToUrl).to.eq(`${Cypress.config().baseUrl}/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8000%2Fwp-admin%2F&reauth=1`);
+        expect(resp.redirectedToUrl).to.eq(
+          `${
+            Cypress.config().baseUrl
+          }/wp-login.php?redirect_to=${encodeURIComponent(
+            Cypress.config().baseUrl
+          )}%2Fwp-admin%2F&reauth=1`
+        );
       });
     });
   });
 
-  context('HTML form submission', () => {
+  context("HTML form submission", () => {
     beforeEach(() => {
-      cy.visit('/wp-login.php');
+      cy.visit("/wp-login.php");
     });
 
-    it('displays errors on login', () => {
+    it("displays errors on login", () => {
       cy.wait(500);
       // incorrect username on purpose
-      cy.get('input[name=log]').type('jane.wordpress');
-      cy.get('input[name=pwd]').type('password123{enter}');
+      cy.get("input[name=log]").type("jane.wordpress");
+      cy.get("input[name=pwd]").type("password123{enter}");
 
       // we should have visible errors now
-      cy.get('div#login_error')
-        .should('be.visible')
-        .and('contain', 'Unknown username. Check again or try your email address.');
+      cy.get("div#login_error")
+        .should("be.visible")
+        .and(
+          "contain",
+          "Unknown username. Check again or try your email address."
+        );
 
       // and still be on the same URL
-      cy.url().should('include', '/wp-login.php');
+      cy.url().should("include", "/wp-login.php");
     });
 
-    it('redirects to /wp-admin/ on success', () => {
+    it("redirects to /wp-admin/ on success", () => {
       cy.wait(1000);
-      cy.get('input[name=log]').type(username);
-      cy.get('input[name=pwd]').type(password);
-      cy.get('form').submit();
+      cy.get("input[name=log]").type(username);
+      cy.get("input[name=pwd]").type(password);
+      cy.get("form").submit();
 
       // we should be redirected to /wp-admin/
-      cy.url().should('include', '/wp-admin/');
-      cy.get('.display-name').should('contain', 'Admin');
+      cy.url().should("include", "/wp-admin/");
+      cy.get("#contextual-help-link").should("contain", "Help");
 
       // and our cookie should be set to 'cypress-session-cookie'
-      cy.getCookie('wordpress_test_cookie').should('exist');
+      cy.getCookie("wordpress_test_cookie").should("exist");
     });
   });
 
-  context('HTML form submission with cy.request', () => {
-    it('can bypass the UI and yet still test log in', () => {
+  context("HTML form submission with cy.request", () => {
+    it("can bypass the UI and yet still test log in", () => {
       // oftentimes once we have a proper e2e test around logging in
       // there is NO more reason to actually use our UI to log in users
       // doing so wastes is slow because our entire page has to load,
@@ -86,8 +99,8 @@ describe('Logging In - WordPress Admin', () => {
       // and sets cookies under the hood. This acts exactly as if the requests
       // came from the browser
       cy.request({
-        method: 'POST',
-        url: '/wp-login.php', // baseUrl will be prepended to this url
+        method: "POST",
+        url: "/wp-login.php", // baseUrl will be prepended to this url
         form: true,
         body: {
           log: username,
@@ -96,28 +109,28 @@ describe('Logging In - WordPress Admin', () => {
       });
 
       // just to prove we have a session
-      cy.getCookie('wordpress_test_cookie').should('exist');
+      cy.getCookie("wordpress_test_cookie").should("exist");
     });
   });
 
   context('Reusable "login" custom command', () => {
     beforeEach(() => {
       // login before each test
-      cy.loginByForm(username, password);
+      cy.loginByForm();
     });
 
-    it('can visit /wp-admin/', () => {
+    it("can visit /wp-admin/", () => {
       // after cy.request, the session cookie has been set
       // and we can visit a protected page
-      cy.visit('/wp-admin/');
-      cy.get('.display-name').should('contain', 'Admin');
+      cy.visit("/wp-admin/");
+      cy.get("#contextual-help-link").should("contain", "Help");
     });
 
-    it('can visit /wp-admin/options-general.php and read options', () => {
+    it("can visit /wp-admin/options-general.php and read options", () => {
       // or another protected page
-      cy.visit('/wp-admin/options-general.php');
-      cy.get('.wrap h1').should('contain', 'General Settings');
-      cy.get('input[name="blogname"]').should('have.value', 'WordPress Local SVN');
+      cy.visit("/wp-admin/options-general.php");
+      cy.get(".wrap h1").should("contain", "General Settings");
+      cy.get('input[name="blogname"]').invoke("val").should("not.be.empty");
     });
   });
 });
--- cypress/integration/rest-api.spec.js
+++ cypress/integration/rest-api.spec.js
@@ -1,33 +1,29 @@
-describe('REST API tests', () => {
-  // we can use these values to log in
-  const username = 'admin';
-  const password = 'password';
-
+describe("REST API tests", () => {
   context('Reusable "login" custom command', () => {
     beforeEach(() => {
       // login before each test
-      cy.loginByForm(username, password);
+      cy.loginByForm();
     });
 
-    it('can visit /wp-json/', () => {
+    it("can visit /wp-json/", () => {
       // after cy.request, the session cookie has been set
       // and we can visit a protected page
-      cy.request('wp-json/').then((response) => {
+      cy.request("wp-json/").then((response) => {
         // response.body is automatically serialized into JSON
-        expect(response.body).to.have.property('name', 'WordPress Local SVN');
+        expect(response.body).to.have.property("name", "Test Bed");
       });
     });
 
-    it('can visit wp-json/wp/v2/posts', () => {
+    it("can visit wp-json/wp/v2/posts", () => {
       // after cy.request, the session cookie has been set
       // and we can visit a protected page
-      cy.request('wp-json/wp/v2/posts').then((response) => {
+      cy.request("wp-json/wp/v2/posts").then((response) => {
         // response.body is automatically serialized into JSON
         // how many posts do we have here? Should we account for more?
         expect(response.body.length).to.be.at.least(1);
-        expect(response.body[1]).to.have.property('title');
+        expect(response.body[0]).to.have.property("title");
         // what if we check for a custom property?
-        expect(response.body[1]).to.not.have.property('my_custom_prop');
+        expect(response.body[0]).to.not.have.property("my_custom_prop");
       });
     });
   });
