5c444b771544876c463c9d3dfa3d1e2ffd107f9a
Jake Spurlock <whyisjake@gmail.com>
2022-02-10 11:53:54 -0800

    5.9 updates.

--- cypress/integration/admin/plugin.js
+++ cypress/integration/admin/plugin.js
@@ -1,15 +1,16 @@
-describe('WordPress User Tests', () => {
-  context('Creating a new Editor', () => {
+describe('WordPress Admin Plugin Tests', () => {
+  context('Login ahead of tests', () => {
     beforeEach(() => {
       // login before each test
       cy.loginByForm();
     });
 
-    it('can visit /wp-admin/', () => {
+    it('can find a Hello Dolly lyric', () => {
       // after cy.request, the session cookie has been set
       // and we can visit a protected page
       cy.visit('/wp-admin/');
-      cy.get('#contextual-help-link').should('contain', 'Help');
+      cy.get('#dolly');
+      cy.get('#contextual-help-link').should('be.visible');
     });
   });
 });
