ca12fe25f6322d116f4af4c992da0c68d12a227f
Jake Spurlock <whyisjake@gmail.com>
2022-02-10 13:26:37 -0800

    Simplify

--- cypress/integration/admin/plugin.spec.js
+++ cypress/integration/admin/plugin.spec.js
@@ -1,13 +1,10 @@
 describe('WordPress Admin Plugin Tests', () => {
   context('Login ahead of tests', () => {
     beforeEach(() => {
-      // login before each test
       cy.loginByForm();
     });
 
     it('can find a Hello Dolly lyric', () => {
-      // after cy.request, the session cookie has been set
-      // and we can visit a protected page
       cy.visit('/wp-admin/');
       cy.get('#dolly');
       cy.get('#contextual-help-link').should('be.visible');
